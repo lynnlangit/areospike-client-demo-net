81e41f621093e0975cdbc6a32aca4eb63e67e456
Jim Counts <fritzjob18@nethere.com>
2015-07-29 21:22:16 -0700

    C# Idioms

--- AerospikeClientDemo/AerospikeClientDemo/Program.cs
+++ AerospikeClientDemo/AerospikeClientDemo/Program.cs
@@ -1,108 +1,101 @@
 ﻿using Aerospike.Client;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace AerospikeClientDemo
 {
     public class Program
     {
-
         //Update the IP addresses to the values for YOUR Aerospike instance
-        private static AerospikeClient connect()
+        private static AerospikeClient Connect()
         {
-            Host[] hosts = new Host[] { new Host("192.168.1.11", 3000),
+            var hosts = new[] { new Host("192.168.1.11", 3000),
                 new Host("192.168.1.23", 3000) };
-            AerospikeClient client = new AerospikeClient(new ClientPolicy(), hosts);
+            var client = new AerospikeClient(new ClientPolicy(), hosts);
             return client;
         }
 
-        public static void Main(String[] args)
+        public static void Main(string[] args)
         {
+            var client = Connect();
+            var policy = new Policy();
+            var writePolicy = new WritePolicy();
+            var batchPolicy = new BatchPolicy();
 
-            AerospikeClient client = connect();
-            Policy policy = new Policy();
-            WritePolicy writePolicy = new WritePolicy();
-            BatchPolicy batchPolicy = new BatchPolicy();
-
-            //NOTE: adjust the timeout value depending on your demo machine 
+            //NOTE: adjust the timeout value depending on your demo machine
             writePolicy.timeout = 1000;
-            Key key = new Key("test", "myset", "mykey");
+            var key = new Key("test", "myset", "mykey");
 
-            writeSingleValue(client, writePolicy, key);
-            checkKeyExists(client, policy, key);
-            addSingleValue(client, writePolicy, key);
-            writeMultipleValues(client, writePolicy, key);
-            writeValueWithTTL(client);
+            WriteSingleValue(client, writePolicy, key);
+            CheckKeyExists(client, policy, key);
+            AddSingleValue(client, writePolicy);
+            WriteMultipleValues(client, writePolicy, key);
+            WriteValueWithTtl(client);
 
-            readAllValuesForKey(client, policy, key);
-            readSomeValuesForKey(client, policy, key);
+            ReadAllValuesForKey(client, policy, key);
+            ReadSomeValuesForKey(client, policy, key);
 
-            deleteValue(client, writePolicy, key);
-            deleteRecord(client, writePolicy, key);
+            DeleteValue(client, writePolicy, key);
+            DeleteRecord(client, writePolicy, key);
 
-            addRecords(client, writePolicy);
-            batchReadRecords(client, batchPolicy);
+            AddRecords(client, writePolicy);
+            BatchReadRecords(client, batchPolicy);
 
-            multiOps(client, writePolicy, key);
+            MultiOps(client, writePolicy, key);
 
             client.Close();
         }
 
-        private static void writeSingleValue(AerospikeClient client,
+        private static void WriteSingleValue(AerospikeClient client,
                 WritePolicy writePolicy, Key key)
         {
-            Bin bin = new Bin("mybin", "myReadModifyWriteValue");
+            var bin = new Bin("mybin", "myReadModifyWriteValue");
             client.Put(writePolicy, key, bin);
             Console.WriteLine("Wrote this new value (or bin): " + key);
             Console.WriteLine("");
         }
 
-        private static void checkKeyExists(AerospikeClient client, Policy policy,
+        private static void CheckKeyExists(AerospikeClient client, Policy policy,
                 Key key)
         {
             Console.WriteLine("Check a record exists");
-            bool exists = client.Exists(policy, key);
+            var exists = client.Exists(policy, key);
             Console.WriteLine(key + " exists? " + exists);
             Console.WriteLine("");
         }
 
-        private static void addSingleValue(AerospikeClient client,
-                WritePolicy writePolicy, Key key)
+        private static void AddSingleValue(AerospikeClient client,
+                WritePolicy writePolicy)
         {
-            Key newKey = new Key("test", "myAddSet", "myAddKey");
-            Bin counter = new Bin("mybin", 1);
+            var newKey = new Key("test", "myAddSet", "myAddKey");
+            var counter = new Bin("mybin", 1);
             client.Add(writePolicy, newKey, counter);
             Console.WriteLine("Wrote this additional value (or bin):  " + newKey);
             Console.WriteLine("");
         }
 
-        private static void writeMultipleValues(AerospikeClient client,
+        private static void WriteMultipleValues(AerospikeClient client,
                 WritePolicy writePolicy, Key key)
         {
-            Bin bin0 = new Bin("location", "Oslo");
-            Bin bin1 = new Bin("name", "Lynn");
-            Bin bin2 = new Bin("age", 42);
+            var bin0 = new Bin("location", "Oslo");
+            var bin1 = new Bin("name", "Lynn");
+            var bin2 = new Bin("age", 42);
             client.Put(writePolicy, key, bin0, bin1, bin2);
             Console.WriteLine("Wrote these additional values:  " + key
                     + " " + bin0 + " " + bin1 + " " + bin2);
             Console.WriteLine("");
         }
 
-        private static void writeValueWithTTL(AerospikeClient client)
+        private static void WriteValueWithTtl(AerospikeClient client)
         {
-            WritePolicy writePolicy = new WritePolicy();
-            writePolicy.expiration = 2;
+            var writePolicy = new WritePolicy { expiration = 2 };
 
-            Key ttlKey = new Key("test", "myTtlSet", "myTtlKey");
-            Bin bin = new Bin("gender", "female");
+            var ttlKey = new Key("test", "myTtlSet", "myTtlKey");
+            var bin = new Bin("gender", "female");
             client.Put(writePolicy, ttlKey, bin);
 
-            Policy policy = new Policy();
-            checkKeyExists(client, policy, ttlKey);
+            var policy = new Policy();
+            CheckKeyExists(client, policy, ttlKey);
             Console.WriteLine("sleeping for 4 seconds");
             try
             {
@@ -112,83 +105,82 @@ namespace AerospikeClientDemo
             {
                 Console.WriteLine(e + "");
             }
-            checkKeyExists(client, policy, ttlKey);
+            CheckKeyExists(client, policy, ttlKey);
             Console.WriteLine("");
         }
 
-        private static void readAllValuesForKey(AerospikeClient client, Policy policy, Key key)
+        private static void ReadAllValuesForKey(AerospikeClient client, Policy policy, Key key)
         {
             Console.WriteLine("Read all bins of a record");
-            Record record = client.Get(policy, key);
+            var record = client.Get(policy, key);
             Console.WriteLine("Read these values: " + record);
             Console.WriteLine("");
         }
 
-        private static void readSomeValuesForKey(AerospikeClient client, Policy policy, Key key)
+        private static void ReadSomeValuesForKey(AerospikeClient client, Policy policy, Key key)
         {
             Console.WriteLine("Read specific values (or bins) of a record");
-            Record record = client.Get(policy, key, "name", "age");
+            var record = client.Get(policy, key, "name", "age");
             Console.WriteLine("Read these values: " + record);
             Console.WriteLine("");
         }
 
-        private static void deleteValue(AerospikeClient client,
+        private static void DeleteValue(AerospikeClient client,
                 WritePolicy writePolicy, Key key)
         {
-            Bin bin1 = Bin.AsNull("mybin");
+            var bin1 = Bin.AsNull("mybin");
             client.Put(writePolicy, key, bin1);
             Console.WriteLine("Deleted this value:  " + bin1);
             Console.WriteLine("");
         }
 
-        private static void deleteRecord(AerospikeClient client,
+        private static void DeleteRecord(AerospikeClient client,
                 WritePolicy policy, Key key)
         {
             client.Delete(policy, key);
-            checkKeyExists(client, policy, key);
+            CheckKeyExists(client, policy, key);
             Console.WriteLine("Deleted this record: " + key);
             Console.WriteLine("");
         }
 
-        private static void addRecords(AerospikeClient client,
+        private static void AddRecords(AerospikeClient client,
                 WritePolicy writePolicy)
         {
-            int size = 1024;
-            for (int i = 0; i < size; i++)
+            const int size = 1024;
+            for (var i = 0; i < size; i++)
             {
-                Key key = new Key("test", "myset", (i + 1));
+                var key = new Key("test", "myset", (i + 1));
                 client.Put(writePolicy, key, new Bin("dots", i + " dots"));
             }
             Console.WriteLine("Added " + size + " Records");
             Console.WriteLine("");
         }
 
-        private static void batchReadRecords(AerospikeClient client, BatchPolicy batchPolicy)
+        private static void BatchReadRecords(AerospikeClient client, BatchPolicy batchPolicy)
         {
             Console.WriteLine("Batch Reads");
-            int size = 1024;
-            Key[] keys = new Key[size];
-            for (int i = 0; i < keys.Length; i++)
+            const int size = 1024;
+            var keys = new Key[size];
+            for (var i = 0; i < keys.Length; i++)
             {
                 keys[i] = new Key("test", "myset", (i + 1));
             }
-            Record[] records = client.Get(batchPolicy, keys);
+            var records = client.Get(batchPolicy, keys);
             Console.WriteLine("Read " + records.Length + " records");
         }
 
-        private static void multiOps(AerospikeClient client,
+        private static void MultiOps(AerospikeClient client,
                 WritePolicy writePolicy, Key key)
         {
             Console.WriteLine("Multiops");
-            Bin bin1 = new Bin("optintbin", 7);
-            Bin bin2 = new Bin("optstringbin", "string value");
+            var bin1 = new Bin("optintbin", 7);
+            var bin2 = new Bin("optstringbin", "string value");
             client.Put(writePolicy, key, bin1, bin2);
-            Bin bin3 = new Bin(bin1.name, 4);
-            Bin bin4 = new Bin(bin2.name, "new string");
-            Record record = client.Operate(writePolicy, key, Operation.Add(bin3),
+            var bin3 = new Bin(bin1.name, 4);
+            var bin4 = new Bin(bin2.name, "new string");
+            var record = client.Operate(writePolicy, key, Operation.Add(bin3),
                     Operation.Put(bin4), Operation.Get());
             Console.WriteLine("Record: " + record);
         }
-
     }
 }
